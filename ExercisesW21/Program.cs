45aaa32d755ddd0ff8d76d16ef4dba65f3fc7274
Marta Stefanska <marta.stefanska@outlook.es>
2024-02-25 19:41:02 +0100

    zad 5 z pętlą do-while i warunkiem wyjścia

--- ExercisesW21/Program.cs
+++ ExercisesW21/Program.cs
@@ -1,25 +1,34 @@
-﻿
-Console.WriteLine("Podaj liczbę całkowitą: ");
-var numberAsString = Console.ReadLine();
-
-try
+﻿do
 {
-    int number = int.Parse(numberAsString);
-    char[] lettersAsNumber = numberAsString.ToArray();
-    Console.WriteLine("Wynik dla liczby: " + numberAsString);
+    Console.WriteLine("Podaj liczbę całkowitą lub wpisz 'q' aby wyjść: ");
+    var input = Console.ReadLine();
 
-    for (char digit = '0'; digit <= '9'; digit++)
-    { 
-        int count = 0;
-        foreach (char letterAsNumber in lettersAsNumber)
-        {
-            if (letterAsNumber == digit)
-            { count++; }
-        }
-        Console.WriteLine($"{digit} => {count}");
+    if (input.ToLower() == "q" )
+    {
+        Console.WriteLine("Koniec programu.");
+        break;
     }
-}
-catch
-{
-    Console.WriteLine("Podany ciąg nie jest liczbą całkowitą");
-}
+
+    try
+    {
+        int number = int.Parse(input);
+        char[] lettersAsNumber = input.ToArray();
+        Console.WriteLine("Wynik dla liczby: " + input);
+
+        for (char digit = '0'; digit <= '9'; digit++)
+        {
+            int count = 0;
+            foreach (char letterAsNumber in lettersAsNumber)
+            {
+                if (letterAsNumber == digit)
+                { count++; }
+            }
+            Console.WriteLine($"{digit} => {count}");
+        }
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Podany ciąg nie jest liczbą całkowitą. Spróbuj ponownie lub wpisz 'q' aby wyjść");
+    }
+}while(true);
