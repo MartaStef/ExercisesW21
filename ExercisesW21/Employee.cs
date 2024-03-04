d77eacade1be01abb7d963f3c65728977701aadb
Marta Stefanska <marta.stefanska@outlook.es>
2024-03-04 18:25:13 +0100

    zadanie dzień 6 rozróżnienie płci w wyniku

---
+++ ExercisesW21/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ExercisesW21
+{
+    public class Employee
+    {
+        private List<int> grades = new List<int>();
+        public Employee(string name, string surname, int age, char sex)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Sex = sex;
+        }
+        public string Name { get; private set; }
+        public string Surname { get;private set; }
+        public int Age { get; private set;}
+        public char Sex { get; private set;}
+
+        public int Result
+        { 
+            get
+            { 
+                return this.grades.Sum(); 
+            }
+        }
+
+        public void AddGrade(int grade) 
+        { 
+        this.grades.Add(grade);
+        }
+    }
+}
--- ExercisesW21/Program.cs
+++ ExercisesW21/Program.cs
@@ -1,34 +1,43 @@
-﻿while(true)
+﻿using ExercisesW21;
+
+Employee employee1 = new Employee("Rutger","Hauer", 80, 'm');
+Employee employee2 = new Employee("Arnold", "Schwarz", 77, 'm');
+Employee employee3 = new Employee("Julia", "Roberts", 57, 'f');
+
+employee1.AddGrade(5);
+employee1.AddGrade(3);
+employee1.AddGrade(4);
+
+employee2.AddGrade(5);
+employee2.AddGrade(8);
+employee2.AddGrade(7);
+employee2.AddGrade(9);
+
+employee3.AddGrade(10);
+employee3.AddGrade(11);
+
+List<Employee> employees = new List<Employee>()
+{ employee1, employee2, employee3 };
+
+int maxResult = 0;
+
+Employee bestEmployee = null;
+
+foreach (var employee in employees)
 {
-    Console.WriteLine("Podaj liczbę całkowitą lub wpisz 'q' aby wyjść: ");
-    var input = Console.ReadLine();
-
-    if (input.ToLower() == "q" )
+    if (employee.Result > maxResult)
     {
-        Console.WriteLine("Koniec programu.");
-        break;
-    }
-
-    try
-    {
-        int number = int.Parse(input);
-        char[] lettersAsNumber = input.ToArray();
-        Console.WriteLine("Wynik dla liczby: " + input);
-
-        for (char digit = '0'; digit <= '9'; digit++)
-        {
-            int count = 0;
-            foreach (char letterAsNumber in lettersAsNumber)
-            {
-                if (letterAsNumber == digit)
-                { count++; }
-            }
-            Console.WriteLine($"{digit} => {count}");
-        }
-        break;
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("Podany ciąg nie jest liczbą całkowitą. Spróbuj ponownie lub wpisz 'q' aby wyjść");
+        bestEmployee = employee;
+        maxResult = employee.Result;
     }
 }
+
+string text = " ";
+
+if (bestEmployee.Sex == 'm') { text = ", uzyskal wynik "; }
+if (bestEmployee.Sex == 'f') { text = ", uzyskała wynik "; }
+
+Console.WriteLine($"Najlepszy pracownik: " + bestEmployee.Name + " " + bestEmployee.Surname + ", lat " +
+bestEmployee.Age + text + bestEmployee.Result + " punktow"); 
+    
+
