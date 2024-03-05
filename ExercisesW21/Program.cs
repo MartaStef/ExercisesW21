73c09ab11711c5683bffc459a6ae16e8819cbbe8
Marta Stefanska <marta.stefanska@outlook.es>
2024-03-05 19:21:55 +0100

    zadanie dzien 6 dodawanie ocen na konsoli

--- ExercisesW21/Program.cs
+++ ExercisesW21/Program.cs
@@ -3,21 +3,23 @@
 Employee employee1 = new Employee("Rutger","Hauer", 80, 'm');
 Employee employee2 = new Employee("Arnold", "Schwarz", 77, 'm');
 Employee employee3 = new Employee("Julia", "Roberts", 57, 'f');
-
-employee1.AddGrade(5);
-employee1.AddGrade(3);
-employee1.AddGrade(4);
-
-employee2.AddGrade(5);
-employee2.AddGrade(8);
-employee2.AddGrade(7);
-employee2.AddGrade(9);
-
-employee3.AddGrade(10);
-employee3.AddGrade(11);
+Employee employee4 = new Employee("Johnny", "Depp", 60, 'm');
 
 List<Employee> employees = new List<Employee>()
-{ employee1, employee2, employee3 };
+{ employee1, employee2, employee3, employee4 };
+
+foreach (var employee in employees)
+{
+     while(true)
+     {
+        Console.WriteLine("Podaj ocenę pracownika:" + employee.Name);
+        var input = Console.ReadLine();
+        if (input == "next")
+        { break; }
+        var grade = int.Parse(input);
+        employee.AddGrade(grade);
+     } 
+}
 
 int maxResult = 0;
 
@@ -39,5 +41,4 @@ if (bestEmployee.Sex == 'f') { text = ", uzyskała wynik "; }
 
 Console.WriteLine($"Najlepszy pracownik: " + bestEmployee.Name + " " + bestEmployee.Surname + ", lat " +
 bestEmployee.Age + text + bestEmployee.Result + " punktow"); 
-    
-
+ 
